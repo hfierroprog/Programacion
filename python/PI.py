cc83edab9c065c53fff0a67483299f6aac5480f0
hfierroprog <hfierroprog@gmail.com>
2017-05-21 18:13:53 -0400

    Cambios..

---
+++ python/PI.py
@@ -0,0 +1,16 @@
+def main():
+	num = 1
+	den = 1
+	suma = 0
+	n = 100000
+	cont = 0;
+
+	while (cont < n):
+		suma += num / den
+		den+=2
+		suma -= num / den
+		den+=2
+		cont +=1
+
+	print (suma*4)
+main()
